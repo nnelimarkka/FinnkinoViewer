9733da2d163f3e63d238cf761b3dcb317fc3a036
Niklas Nelimarkka <nikkeneli@gmail.com>
2020-03-16 17:00:55 +0200

    commit 9.1

--- app/src/main/java/my/app/finnkinoviewer/MainActivity.java
+++ app/src/main/java/my/app/finnkinoviewer/MainActivity.java
@@ -3,12 +3,20 @@ package my.app.finnkinoviewer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.Kinos));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(myAdapter);
     }
 }
